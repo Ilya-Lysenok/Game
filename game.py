fad16318adfb42c45a3c5e1f67bead2ea7c5a4b9
Илья Лысенок <101513077+Ilya-Lysenok@users.noreply.github.com>
2023-06-11 17:07:56 +0300

    Update game.py

--- game.py
+++ game.py
@@ -5,3 +5,24 @@ init()
 # Глобальные переменные (настройки)
 win_width = 1280 
 win_height = 720
+
+class GameSprite(sprite.Sprite):
+  # конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # Вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.width = size_x
+        self.height = size_y
+
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+  # метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
