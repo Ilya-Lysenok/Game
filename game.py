7cda436c49a6f85a36118573d7e59d330fe794b9
Илья Лысенок <101513077+Ilya-Lysenok@users.noreply.github.com>
2023-06-11 17:10:25 +0300

    Update game.py

--- game.py
+++ game.py
@@ -26,3 +26,21 @@ class GameSprite(sprite.Sprite):
   # метод, отрисовывающий героя на окне
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+     
+#класс-наследник для спрайта-игрока (управляется стрелками)
+class Player(GameSprite):
+    def update_l_r(self):
+        global f
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+            f=1
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+            f=0
+    def update_a_d(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
