88207eb3a680cc049f6ccdcb1c5cc3d94d717ce5
Илья Лысенок <101513077+Ilya-Lysenok@users.noreply.github.com>
2023-06-11 17:53:59 +0300

    Update game.py

--- game.py
+++ game.py
@@ -131,6 +131,9 @@ for r in level:
                 platform = GameSprite('platform.png',x, y, 40, 40 ,0)
                 platforms.append(platform)
                 items.add(platform)
+            if c=='s':
+                hero = Player('images/sprite.png', x, y,40,40, 4)
+                items.add(hero)
             if c=='c':
                 monster = Enemy('monster_crow.png', x, y,40,40, 2,'left')
                 monsters.append(monster)
